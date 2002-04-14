a35936f9ab5bf697ae528317c6b6b22e3de6320a
ma810 <ma810>
2002-04-14 23:05:29 +0000

    no message

---
+++ server/librarian/LibrarianRequest.java
@@ -0,0 +1,37 @@
+/*
+ * LibrarianRequest.java
+ *
+ * Copyright (c) 2002: The Trustees of Columbia University
+ * in the City of New York.  All Rights Reserved.
+ */
+
+package psl.chime4.data.server.librarian;
+
+/**
+ * Encapsulates a librarian search request, which currently consists of only a
+ * query string but may eventually include other fields that constrain the
+ * search domain.
+ *
+ * @author Mark Ayzenshtat 
+ */
+public class LibrarianRequest {
+	private String mQuery;
+	
+	/**
+	 * Retrieves the search query.
+	 *
+	 * @return the search query
+	 */
+	public String getQuery() {
+		return mQuery;
+	}
+	
+	/**
+	 * Assigns the search query.
+	 *
+	 * @param iQuery the search query
+	 */
+	public void setQuery(String iQuery) {
+		mQuery = iQuery;
+	}
+}
---
+++ server/librarian/LibrarianResult.java
@@ -0,0 +1,54 @@
+/*
+ * LibrarianResult.java
+ *
+ * Copyright (c) 2002: The Trustees of Columbia University
+ * in the City of New York.  All Rights Reserved.
+ */
+
+package psl.chime4.data.server.librarian;
+
+/**
+ * Encapsulates a librarian search result.
+ *
+ * @author Mark Ayzenshtat 
+ */
+public class LibrarianResult {
+	private LibrarianRequest mSourceRequest;
+	private ResourceDescriptor[] mSearchResults;
+	
+	/**
+	 * Retrieves the search request that is the source of this result.
+	 *
+	 * @return the source search request
+	 */
+	public LibrarianRequest getSourceRequest() {
+		return mSourceRequest;
+	}
+
+	/**
+	 * Assigns the search request that is the source of this result.
+	 *
+	 * @param iRequest the source search request
+	 */	
+	public void setSourceRequest(LibrarianRequest iRequest) {
+		mSourceRequest = iRequest;
+	}
+
+	/**
+	 * Retrieves the search results.
+	 *
+	 * @return the search results
+	 */	
+	public ResourceDescriptor[] getSearchResults() {
+		return mSearchResults;
+	}
+	
+	/**
+	 * Assigns the search results.
+	 *
+	 * @param iResults the search results
+	 */	
+	public void setSearchResults(ResourceDescriptor[] iResults) {
+		mSearchResults = iResults;
+	}
+}
