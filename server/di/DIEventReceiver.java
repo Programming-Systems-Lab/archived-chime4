b56b53aac21c2a4b3a6ca22303191a1c26339b4e
ma810 <ma810>
2002-05-09 04:24:15 +0000

    Added "public" to the interface declaration so's we can implement it outside the package.

--- server/di/DIEventReceiver.java
+++ server/di/DIEventReceiver.java
@@ -1,6 +1,6 @@
 package psl.chime4.server.di;
 
-interface DIEventReceiver
+public interface DIEventReceiver
 {
     public void receiveEvent( DIEvent event );
     public void receiveResult( DIHost result );
