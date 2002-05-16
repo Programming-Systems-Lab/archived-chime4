de70eeb9bbd29409e9394e1254fde931d8b573b5
buko <buko>
2002-05-16 07:02:53 +0000

    no message

--- server/cebs/EventSystem.java
+++ server/cebs/EventSystem.java
@@ -3,7 +3,7 @@ package psl.chime4.server.cebs;
 /**
  * An EventSystemInterface object provides access to the underlying mechanism
  * for sending and recieving events. It abstracts the underlying transport
- * protocol used for sending and recieving events in a publish/suscribe 
+ * protocol used for sending and recieving events in a publish/suscribe
  * event system.
  * <p>
  * If the underlying event protocol allows it, a network entity can recieve
@@ -14,81 +14,52 @@ package psl.chime4.server.cebs;
  **/
 public abstract class EventSystem
 {
-    /** the UID of the network entity using this event system **/
-    protected int sourceUID;
-    
-    /** blocking queue for recieving incoming events **/
-    protected BlockingEventQueue eventQueue = new BlockingEventQueue();
-    
-    /** the event dispatcher thread used to dispatch events **/
-    private EventDispatcherThread dispatchThread;
-    
-    /** event reciever used to manage the recieving of events **/
-    protected EventReciever eventReciever;
-    
-    /** event sender used to manage the sending of events **/
-    protected EventSender eventSender;
-    
-    /**
-     * Construct an EventSystem interface object with a given sourceUID.
-     *
-     * @param sourceUID user ID of the entity using this event system
-     **/
-    protected EventSystem(int sourceUID)
-    {
-        this.sourceUID = sourceUID;
-        // setup the event dispatcher thread
-        dispatchThread = new EventDispatcherThread(eventQueue, 
-                                                   getEventReciever());
-    }
-    
-    /**
-     * Start the event system. Once this method is called you can begin
-     * recieving events.
-     *
-     * @throws CEBSException 
-     *         if the event system could not be started for some reason
-     **/
-    public void startup() throws CEBSException
-    {
-        // start the dispatcher thread
-        dispatchThread.startup();
-    }
-    
-    /**
-     * Shutdown the event system. You will no longer recieve events or be
-     * able to send events.
-     **/
-    public void shutdown()
-    {
-        // shutdown the dispatcher thread
-        dispatchThread.shutdown();
-    }
-    
-    /**
-     * Get the EventSender that can be used to send events.
-     *
-     * @return EventSender used to send events.
-     **/
-    public EventSender getEventSender()
-    {
-        return eventSender;
-    }
-    
+   /** blocking queue for recieving incoming events **/
+   private BlockingEventQueue eventQueue = new BlockingEventQueue();
+   
+   /** the event dispatcher thread used to dispatch events **/
+   private EventDispatcherThread dispatchThread;
+   
+   /** amount of time a connection has been made to a server **/
+   private CounterMap connectionCount = new CounterMap();
+   
    /**
-    * Get the EventReciever that can be used for recieving events.
-    *
-    * @return EventReciever used to recieve events.
+    * Construct an EventSystem interface object. This method is protected to
+    * prevent client instantiation.
     **/
-    public EventReciever getEventReciever()
-    {
-        return eventReciever;
-    }
-    
-    /**
-     * Create an empty Event.
-     *
-     * @return an empty Event
-     **/
-    public abstract Event createEmptyEvent();
+   protected EventSystem()
+   {
+      ; // do nothing
+   }
+   
+   /**
+    * Start the event system. Once this method is called you can begin
+    * recieving events.
+    *
+    * @throws CEBSException
+    *         if the event system could not be started for some reason
+    **/
+   public void startup() throws CEBSException
+   {
+      // start the dispatcher thread
+      dispatchThread = new EventDispatcherThread(this);
+      dispatchThread.startup();
+   }
+   
+   /**
+    * Shutdown the event system. You will no longer recieve events or be
+    * able to send events.
+    **/
+   public void shutdown()
+   {
+      // shutdown the dispatcher thread
+      dispatchThread.shutdown();
+   }
+   
+   /**
+    * Create an empty Event.
+    *
+    * @return an empty Event
+    **/
+   public abstract Event createEmptyEvent();
 }
