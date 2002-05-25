5a132b777f95dffbeaf6557440179cd2a213e730
vs299 <vs299>
2002-05-25 00:37:13 +0000

    enabled binding of VemMapDAO to JdbcVemMapDAO

--- server/data/JdbcDAOFactory.java
+++ server/data/JdbcDAOFactory.java
@@ -37,7 +37,7 @@ public class JdbcDAOFactory implements DAOFactory {
 			JdbcResourceDescriptorDAO.class);
 		
 		// VemMap
-		//bindPersistentToDAO(VemMap.class, JdbcVemMapDAO.class);
+		bindPersistentToDAO(VemMap.class, JdbcVemMapDAO.class);
 		
 		// AuthTicket
 		bindPersistentToDAO(AuthTicket.class, JdbcAuthTicketDAO.class);
