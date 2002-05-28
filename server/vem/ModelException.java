01513d36ecb72b74aa8aa2f6af1e67aea79efbef
vs299 <vs299>
2002-05-28 21:26:04 +0000

    no message

---
+++ server/vem/ModelException.java
@@ -0,0 +1,31 @@
+/*
+ * ModelException.java
+ *
+ * Created on May 28, 2002, 4:03 PM
+ */
+
+package psl.chime4.server.vem;
+
+/**
+ * Indicates that an exception occured while determining a model for an object
+ * or retrieving data about the model.
+ *
+ * @author  Vladislav Shchogolev
+ */
+public class ModelException extends java.lang.Exception {
+    
+    /**
+     * Creates a new instance of <code>ModelException</code> without detail message.
+     */
+    public ModelException() {
+    }
+    
+    
+    /**
+     * Constructs an instance of <code>ModelException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public ModelException(String msg) {
+        super(msg);
+    }
+}
